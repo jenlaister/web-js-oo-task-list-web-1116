9465a2b6dc6b4e776a38961121ad0a7dc056a9df
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-24 21:51:25 -0500

    Done

--- js/models/list.js
+++ js/models/list.js
@@ -1,3 +1,24 @@
 'use strict';
 // List Model
 
+function List(title) {
+  this.title = title
+  List.all.push(this)
+  this.id = List.all.length - 1
+  this.tasks = []
+}
+
+List.prototype.listEl = function() {
+  return `<div class="list"><h2><button class="destroy-list">x</button> ${this.title}</h2><ul id="list-${this.id}" data-id="${this.id}"></ul></div>`
+}
+
+List.prototype.optionEl = function() {
+  return `<option value="${this.id}">${this.title}</option>`
+}
+
+List.prototype.build = function() {
+  $('#select_list').append(this.optionEl())
+  $('#lists').append(this.listEl())
+}
+
+List.all = []
